2f199799550185274a7883f2407e6d33bb924cf6
Jamil Cerrón <luis.cerron.2010@gmail.com>
2023-10-09 17:40:15 -0500

    Añadida la clase círculo Triángulo

---
+++ Trabajo7/Triángulo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabajo7
+{
+    internal class Triángulo
+    {
+        float Base;
+        float altura;
+
+        public Triángulo(float Base, float altura)
+        {
+            this.Base = Base;
+            this.altura = altura;
+        }
+
+        public float RetornaÁreaTriangular(float Base, float altura)
+        {
+            return altura*Base/2;
+        }
+    }
+}
