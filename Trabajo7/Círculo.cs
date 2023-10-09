4e7cac98bd319bf6e804a42a2581fbc50d10fbcd
Jamil Cerrón <luis.cerron.2010@gmail.com>
2023-10-09 17:35:26 -0500

    Añadida la clase círculo

---
+++ Trabajo7/Círculo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabajo7
+{
+    internal class Círculo
+    {
+        private float radio;
+
+
+        public Círculo(float r) 
+        {
+            this.radio = r;
+        }
+
+
+        public float ÁreaDelCírculo(float radio)
+        {
+            return radio * radio;
+        }
+    }
+
+    
+}
