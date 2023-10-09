f30d9120fa2adbd743d720b2793dd6b26910ecef
Jamil Cerrón <luis.cerron.2010@gmail.com>
2023-10-09 17:16:04 -0500

    Añadido la clase rectángulo

---
+++ Trabajo7/Rectángulo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabajo7
+{
+    internal class Rectángulo
+    {
+       float Base;
+       float altura;
+
+        public Rectángulo(float a, float b)
+        {
+            this.altura = a;
+            this.Base = b;
+        }
+
+       public float ÁreaDelRectángulo(int altura, int Base)
+        {
+             return altura * Base;
+        }
+    }
+}
